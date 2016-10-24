e8115cf3fa632bb5b69fe6fb0df7897ad9c7b0d8
Clark Chambers <Clark Chambers>
2016-10-23 19:54:41 -0500

    Working on cool stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Board board;
+
+
+
+	// Use this for initialization
+	void Awake() 
+	{
+		board = GetComponent<Board>();
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		board.SetupScene();
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
